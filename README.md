0d1d511268a2a449865a3b937a94d4c1020ea1ba
Nick Lockwood <nick@charcoaldesign>
2012-01-16 14:49:17 +0000

    First release

---
+++ README.md
@@ -0,0 +1,62 @@
+Purpose
+--------------
+
+Base64 is a set of categories that provide methods to encode and decode data as a base-64-encoded string.
+
+
+Supported OS & SDK Versions
+-----------------------------
+
+* Supported build target - iOS 5.0 / Mac OS 10.7 (Xcode 4.2, Apple LLVM compiler 3.0)
+* Earliest supported deployment target - iOS 4.3 / Mac OS 10.6
+* Earliest compatible deployment target - iOS 3.0 / Mac OS 10.6
+
+NOTE: 'Supported' means that the library has been tested with this version. 'Compatible' means that the library should work on this iOS version (i.e. it doesn't rely on any unavailable SDK features) but is no longer being tested for compatibility and may require tweaking or bug fixes to run correctly.
+
+
+ARC Compatibility
+------------------
+
+Base64 can automatically detect if your project is using ARC or not, and output the correct code using conditional compilation. It will therefore work correctly in either ARC or non-ARC projects without modification.
+
+
+Installation
+--------------
+
+To use the Base64 category in an app, just drag the category files (demo files and assets are not needed) into your project and import the header file into any class where you wish to make use of the Base64 functionality.
+
+
+NSData Extensions
+----------------------
+
+Base64 extends NSData with the following methods:
+
+	+ (NSData *)dataWithBase64EncodedString:(NSString *)string;
+	
+Takes a base-64-encoded string and returns an autoreleased NSData object containing the decoded data. Any non-base-64 characters in the string are ignored, so it is safe to pass a string containing line breaks or other delimiters.
+
+	- (NSString *)base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+	
+Encodes the data as a base-64-encoded string and returns it. The wrapWidth argument allows you to specify the number of characters at which the output should wrap onto a new line. The value of wrapWidth must be a multiple of four. Values that are not a multiple of four will be truncated to the nearest multiple. A value of zero indicates that the data should not wrap.
+	
+	- (NSString *)base64EncodedString;
+	
+Encodes the data as a base-64-encoded string without any wrapping (line breaks).
+
+
+NSString Extensions
+----------------------
+
+Base64 extends NSString with the following methods:
+
+	+ (NSString *)stringWithBase64EncodedString:(NSString *)string;
+	
+Takes a base-64-encoded string and returns an autoreleased NSString object containing the decoded data, interpreted using UTF8 encoding. The vast majority of use cases for Base64 encoding use Ascii or UTF8 strings, so this should be sufficient for most purposes. If you do need to decode string data in an encoding other than UTF8, convert your string to an NSData object first and then use the NSData dataWithBase64EncodedString: method instead.
+
+	- (NSString *)base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+	
+Converts the string data to UTF8 data and then encodes the data as a base-64-encoded string and returns it. The wrapWidth argument allows you to specify the number of characters at which the output should wrap onto a new line. The value of wrapWidth must be a multiple of four. Values that are not a multiple of four will be truncated to the nearest multiple. A value of zero indicates that the data should not wrap.
+	
+	- (NSString *)base64EncodedString;
+	
+Encodes the string as UTF8 data and then encodes that as a base-64-encoded string without any wrapping (line breaks).
---
+++ Tests/UnitTests/DataTests.m
@@ -0,0 +1,108 @@
+//
+//  DataTests.m
+//
+//  Created by Nick Lockwood on 12/01/2012.
+//  Copyright (c) 2012 Charcoal Design. All rights reserved.
+//
+
+#import "DataTests.h"
+#import "NSData+Base64.h"
+
+
+@implementation DataTests
+
+- (void)testOutputEqualsInput
+{
+    //set up data
+    NSString *inputString = @"Hello World!";
+    NSData *inputData = [inputString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //encode
+    NSString *encodedString = [inputData base64EncodedString];
+    NSAssert([encodedString isEqualToString:@"SGVsbG8gV29ybGQh"], @"OutputEqualsInput test failed");
+    
+    //decode
+    NSData *outputData = [NSData dataWithBase64EncodedString:encodedString];
+    NSString *outputString = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]; 
+	NSAssert([outputString isEqualToString:inputString], @"OutputEqualsInput test failed");
+}
+
+- (void)testWrappedInput
+{
+	NSInteger wrapWidth = 8; // must be multiple of 4
+    
+    //set up data
+    NSString *inputString = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac quam non ipsum vestibulum porta ac id ipsum. Morbi sed enim purus. Fusce elementum dui sed nisi tincidunt feugiat interdum felis malesuada. Sed ipsum enim, mollis id adipiscing eu, eleifend eget neque. Nulla quis nunc a nunc pellentesque adipiscing nec non leo. Morbi ut nisi sem, in cursus erat. Praesent eu nibh justo, nec suscipit nisl. Suspendisse eget elit tellus, at rhoncus urna. Quisque imperdiet lobortis sagittis. Duis congue venenatis faucibus. Sed fermentum, ligula venenatis molestie mattis, sem justo ultrices turpis, mattis condimentum quam nunc eu purus. Nulla facilisi. Donec sodales nulla sed diam iaculis quis suscipit quam rhoncus. Nam tincidunt dui sit amet tortor posuere non interdum felis venenatis. In sollicitudin cursus felis vitae convallis. Etiam justo elit, vestibulum sit amet fringilla vel, vestibulum et ante.";
+    
+    NSData *inputData = [inputString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //encode
+    NSString *encodedString = [inputData base64EncodedStringWithWrapWidth:wrapWidth];
+    
+    //wrap width
+    NSArray *lines = [encodedString componentsSeparatedByString:@"\r\n"];
+    NSAssert([[lines valueForKeyPath:@"@max.length"] intValue] == wrapWidth, @"WrappedInput test failed");
+    
+    //decode
+    NSData *outputData = [NSData dataWithBase64EncodedString:encodedString];
+    NSString *outputString = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]; 
+	NSAssert([outputString isEqualToString:inputString], @"WrappedInput test failed");
+}
+
+- (void)testNilInput
+{
+    NSData *data = [NSData dataWithBase64EncodedString:nil];
+	NSAssert(data == nil, @"NilInput test failed");
+}
+
+- (void)testInvalidInput
+{
+    NSData *data = [NSData dataWithBase64EncodedString:@"!!!!!!"];
+	NSAssert(data == nil, @"InvalidInput test failed");
+}
+
+- (void)testShortInput
+{
+    NSString *string = [[@"" dataUsingEncoding:NSUTF8StringEncoding] base64EncodedString];
+	NSAssert(string == nil, @"InvalidInput test failed");
+    
+    string = [[@"A" dataUsingEncoding:NSUTF8StringEncoding] base64EncodedString];
+	NSAssert([string isEqualToString:@"QQ=="], @"InvalidInput test failed");
+    
+    string = [[@"AB" dataUsingEncoding:NSUTF8StringEncoding] base64EncodedString];
+	NSAssert([string isEqualToString:@"QUI="], @"InvalidInput test failed");
+}
+
+- (void)testEdgeCase1
+{
+    //set up data (11 chars)
+    NSString *inputString = @"Hello World";
+    NSData *inputData = [inputString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //encode
+    NSString *encodedString = [inputData base64EncodedString];
+    NSAssert([encodedString isEqualToString:@"SGVsbG8gV29ybGQ="], @"EdgeCase1 test failed");
+    
+    //decode
+    NSData *outputData = [NSData dataWithBase64EncodedString:encodedString];
+    NSString *outputString = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]; 
+	NSAssert([outputString isEqualToString:inputString], @"EdgeCase1 test failed");
+}
+
+- (void)testEdgeCase2
+{
+    //set up data (10 chars)
+    NSString *inputString = @"Hello Worl";
+    NSData *inputData = [inputString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //encode
+    NSString *encodedString = [inputData base64EncodedString];
+    NSAssert([encodedString isEqualToString:@"SGVsbG8gV29ybA=="], @"EdgeCase2 test failed");
+    
+    //decode
+    NSData *outputData = [NSData dataWithBase64EncodedString:encodedString];
+    NSString *outputString = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]; 
+	NSAssert([outputString isEqualToString:inputString], @"EdgeCase2 test failed");
+}
+
+@end
