nicklockwood/Base64

commit 4062db2635859c26facd3fa32776216fb848250c

arm64 support

Base64/Base64.m
@@ -56,19 +56,19 @@
     };
     
     NSData *inputData = [string dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-    long long inputLength = [inputData length];
+    NSUInteger inputLength = [inputData length];
     const unsigned char *inputBytes = [inputData bytes];
     
-    long long maxOutputLength = (inputLength / 4 + 1) * 3;
+    NSUInteger maxOutputLength = (inputLength / 4 + 1) * 3;
     NSMutableData *outputData = [NSMutableData dataWithLength:maxOutputLength];
     unsigned char *outputBytes = (unsigned char *)[outputData mutableBytes];
 
-    int accumulator = 0;
-    long long outputLength = 0;
+    NSUInteger accumulator = 0;
+    NSUInteger outputLength = 0;
     unsigned char accumulated[] = {0, 0, 0, 0};
-    for (long long i = 0; i < inputLength; i++)
+    for (NSUInteger i = 0; i < inputLength; i++)
     {
-        unsigned char decoded = lookup[inputBytes[i] & 0x7F];
+				unsigned char decoded = lookup[inputBytes[i] & 0x7F];
         if (decoded != 99)
         {
             accumulated[accumulator] = decoded;
@@ -99,15 +99,15 @@
     
     const char lookup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     
-    long long inputLength = [self length];
+    NSUInteger inputLength = [self length];
     const unsigned char *inputBytes = [self bytes];
     
-    long long maxOutputLength = (inputLength / 3 + 1) * 4;
+    NSUInteger maxOutputLength = (inputLength / 3 + 1) * 4;
     maxOutputLength += wrapWidth? (maxOutputLength / wrapWidth) * 2: 0;
     unsigned char *outputBytes = (unsigned char *)malloc(maxOutputLength);
     
-    long long i;
-    long long outputLength = 0;
+    NSUInteger i;
+    NSUInteger outputLength = 0;
     for (i = 0; i < inputLength - 2; i += 3)
     {
         outputBytes[outputLength++] = lookup[(inputBytes[i] & 0xFC) >> 2];

commit f105fab1a1d211205c18c8ec6ddbe007669d8873

Fix SIGSEGV when encoding 1- or 2-byte data
Fix bug introduced by 4062db26 (arm64 support), which switched from signed to unsigned counters. It broke the for loop for small inputLength, since the comparison we did (0 < 1lu - 2lu) required signed integers.

Base64/Base64.m
@@ -108,7 +108,7 @@
     
     NSUInteger i;
     NSUInteger outputLength = 0;
-    for (i = 0; i < inputLength - 2; i += 3)
+    for (i = 0; i + 2 < inputLength; i += 3)
     {
         outputBytes[outputLength++] = lookup[(inputBytes[i] & 0xFC) >> 2];
         outputBytes[outputLength++] = lookup[((inputBytes[i] & 0x03) << 4) | ((inputBytes[i + 1] & 0xF0) >> 4)];